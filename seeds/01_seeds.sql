fe44109bef5e8d66235649c64b312026cf00457a
michael <michael.james.ashworth@gmail.com>
2022-01-06 20:19:29 +0000

    Add fake data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,52 @@
+INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    
+INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES (
+    'Aside age', 'Printed sure steady pencil power clearly we now swimming put mouth refer brush managed she crew related completely wait surrounded explore bone teach adult history case raise leave band fog check function cut material gas act enjoy broke truth farm hungry monkey mission line piano coal kids behavior apple physical cave member wide straw thousand aboard writer goose repeat keep greater open hurt plate unhappy finger drop single brown floating standard moment opposite skin tie settlers whose we having merely notice manner pure gentle sentence sharp does forest temperature first wise longer beneath pick chamber together rising suggest grown other hope dead sale setting muscle prevent knowledge harbor according combination finger pressure summer method studying higher over strike discover pencil age mouth syllable truck few hungry explanation while to once sitting tool open industrial six clock damage seen select birthday swam dug gentle become summer slip army pot fox pocket ask recently temperature pack plastic stiff cup glad one hit lucky information separate plant tank lack split further greatly parent island note island share remove wonder word avoid else rose thousand dinner gone base master tales frame guide product driver sudden sheet taste giant tales around death does grew necessary writer who lot found previous pure hill exclaimed action sheep fox gradually refer organization join principle mean thick best additional source customs fruit realize disappear equal declared fur teacher teeth down symbol cool particles school direct getting till people person bicycle known into bring floor obtain nervous mean hollow safe kept alone win twenty ranch fur former bank function pass you receive made castle some deal needs coast carry shelter sale person vowel mill attempt sight private chapter shoulder yes slow coast more sort certainly course nearer exclaimed fighting whole mass palace brush skin distant explanation noun ability after attached', 339, 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg?auto=compress&cs=tinysrgb&h=350', 35421, 7, 2, 2, true, 'British Columbia', 'North Vancouver', 'Canada', '214 Maki Center', '75510');
+    INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES (
+    'Bat zulu', 'Result return income length ill effect roll usual week surprise branch great sugar sat pattern every officer run simply worry usually yet future race most develop gain sing pictured pull rocky blue truck gulf introduced carry knife letter replace told born force skill medicine shoe farm explore tool flight dirt range signal but hot special therefore morning think electric among managed sky eaten luck noise alive fireplace butter smallest memory leg tired fill better energy plates fifteen wind further hour speed danger gray animal mark price sell universe torn flew increase does late clear tales discovery state collect news music mainly class these favorite good got particles winter choose white ring pilot color twice suggest guard twelve excitement told own sometime put rubbed theory bottom fence difference throat grow far settlers rhythm wet local tried bigger dance require nervous rubber down is receive paper three recently differ thee fifteen many fastened oil ride heading street human meat cutting voice something rabbit border wooden industrial escape unit buffalo identity require beginning eight length relationship country probably am model somewhere forth matter audience needed nine flow black attached rays birds that enter vote loose themselves wire activity glass lunch instrument human creature slope bean dried corn manufacturing dish fought fox team electric lack halfway grass three dozen lose pleasure seeing birthday fifteen mass bear knife explore end concerned visitor plus load meat far tin invented greater national but explain source quick thou all energy save valuable rise exclaimed wagon send been near clothes feathers doubt grown transportation mark go opposite once gift on fall seen feel shelter excited invented doing rock last knew word hard guide whenever plant dug radio ants nuts beat serious whatever cut proud unhappy plenty fun pleasant kill met lesson might circle strip age cool song too sit', 990, 'https://images.pexels.com/photos/2104072/pexels-photo-2104072.jpeg', 'https://images.pexels.com/photos/2104072/pexels-photo-2104072.jpeg?auto=compress&cs=tinysrgb&h=350', 38615, 9, 9, 3, true, 'Manitoba', 'Steinbach', 'Canada', '357 Reja Heights', '30750');
+    INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES (
+    'Mirror attempt', 'Bent second curious town deer branch such remarkable cry south try claws fifth wall blind differ notice managed quarter importance right go broken equipment lesson seeing known kill series back protection temperature smoke bend local leave ill stream adventure missing thousand send move hurried suppose riding brass shop donkey behind continent speak hour heart oldest catch sitting news measure fuel milk our she thing silence coat damage very over easy wrong solid magic church highway him evening write slightly me citizen beat hand government official widely nearer collect gentle tonight mission remember account board diagram field tribe chance level far brain she ants molecular journey refer baseball color afternoon gate was mark leader he concerned twelve breath explore universe compare watch bite figure weather walk large discussion present father close describe pen gain small another swing hungry night explain expect pick see occasionally carried widely dropped said community needle pour mental foot riding dead safe warm with spent left shade silver glad atomic second metal picture barn corn enough screen golden camp citizen package nothing blood swim citizen ball guess correctly paid wore broken pocket tightly case research time heard breeze company fear wealth cup shirt elephant order fire fifteen wheat circle dear path sweet bridge height everyone famous agree square repeat easily yet question which entirely lovely duck forgotten eight throat carefully taste engine bell duck flew salmon men law society seven smile pretty win string treated outside dig safe all term goes gate soon corn anywhere cool manufacturing solution burst cheese far huge piano cook fur fall somewhere consonant offer stand birds independent food aid bottle year degree range package friend tree continued essential support plant herself slide useful win sound being appropriate hidden mirror skill afternoon stronger harder accident later lost twenty whale afternoon probably swim cookies', 629, 'https://images.pexels.com/photos/2104072/pexels-photo-2104072.jpeg', 'https://images.pexels.com/photos/2104072/pexels-photo-2104072.jpeg?auto=compress&cs=tinysrgb&h=350', 73384, 0, 4, 1, true, 'Prince Edward Island', 'Souris', 'Canada', '337 Giljiv Street', '98546');
+    
+INSERT INTO reservations (
+    id, guest_id, property_id, start_date, end_date) 
+    VALUES (
+    1017476, 714, 992, '2021-08-17', '2021-09-09');
+    INSERT INTO reservations (
+    id, guest_id, property_id, start_date, end_date) 
+    VALUES (
+    1017477, 599, 82, '2023-08-18', '2023-08-19');
+    INSERT INTO reservations (
+    id, guest_id, property_id, start_date, end_date) 
+    VALUES (
+    1017478, 474, 902, '2020-03-23', '2020-04-16');
+    
+INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    77, 240, 1017784, 4, 'An juati gimoh ro safhit mafvoed ca unupaboz se oviljev ifa iw buafosa da afiwovar zuwokus usimi rimcu.');
+    INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    750, 14, 1010182, 5, 'Iwijajik ib ha sico ovlopsaf fi haedpa hoz kemol mu runku zozukse wos lojvu jev.');
+    INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    785, 601, 1016644, 4, 'Pimi sa bifunu liim ladnofap eg jihzi gimiwmuj coccaf cofid zop pumze zebul.');
+    
